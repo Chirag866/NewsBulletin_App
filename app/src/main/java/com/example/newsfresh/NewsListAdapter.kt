c7d6c1ab23c090dc0ac33b47566043a7c171baee
Chirag866 <chiraggarg866@gmail.com>
2022-06-22 14:20:27 +0530

    News Bulletin App

---
+++ app/src/main/java/com/example/newsfresh/NewsListAdapter.kt
@@ -0,0 +1,54 @@
+package com.example.newsfresh
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+
+class NewsListAdapter(private val listener: NewsItemClicked): RecyclerView.Adapter<NewsViewHolder>() {
+
+    private val items: ArrayList<News> = ArrayList()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NewsViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_news, parent, false)
+        val viewHolder = NewsViewHolder(view)
+        view.setOnClickListener{
+            listener.onItemClicked(items[viewHolder.adapterPosition])
+        }
+        return viewHolder
+    }
+
+    override fun getItemCount(): Int {
+        return items.size
+    }
+
+    override fun onBindViewHolder(holder: NewsViewHolder, position: Int) {
+        val currentItem = items[position]
+        holder.titleView.text = currentItem.title
+        holder.author.text = currentItem.author
+        Glide.with(holder.itemView.context).load(currentItem.imageUrl).into(holder.image)
+    }
+     // to provide items to the adaptor we will create a function.
+    fun updateNews(updatedNews: ArrayList<News>) {
+        items.clear()
+        items.addAll(updatedNews)
+
+         // now time has come for adaptor to update its item, we do this by the below syntax. which means the three functions
+         // of adaptor namely 1)getItemCount 2)onBindViewHolder 3)updateNews are called again in the adaptor
+        notifyDataSetChanged()
+    }
+}
+
+class NewsViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    val titleView: TextView = itemView.findViewById(R.id.title)
+    // there are coming from item_news.xml.
+    val image: ImageView = itemView.findViewById(R.id.image)
+    val author: TextView = itemView.findViewById(R.id.author)
+}
+
+interface NewsItemClicked {
+    fun onItemClicked(item: News)
+}
